50938912831350beff0e306db392dd07c6ce8eff
SimpleKjell <kjell@simplefox.de>
2018-03-26 14:53:45 +0200

    deaktivieren der Animation

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -13,11 +13,11 @@
   <body class="home">
     @include('inc.navbar')
 
-    <div data-next="mediabrothers" class="transition-layer">
+    <!-- <div data-next="mediabrothers" class="transition-layer">
       <div class="logo">
         <img src="http://www.mediabrothers.at/wp-content/themes/mediabrothers/assets/images/logo.png" alt="">
       </div>
-    </div>
+    </div> -->
     <div class="scroll"></div>
     <div class="masonry"></div>
 

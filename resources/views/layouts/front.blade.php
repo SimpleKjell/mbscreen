2bdf40bcce9576f414e23efd10cb44a9a007a109
kjell-dev <k.weibrecht@vertriebsmedia.de>
2018-06-06 14:42:06 +0200

    removed quote

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -21,7 +21,7 @@
       <div class="logo">
         <img src="http://www.mediabrothers.at/wp-content/themes/mediabrothers/assets/images/logo.png" alt="">
       </div>
-    </div> -->
+    </div>
     <div class="scroll"></div>
     <div class="masonry"></div>
 
